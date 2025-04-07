36e7ec381bd1b972f73679c3d03fd57c9d476e4b
Richard Altman <114539157+Holasyb918@users.noreply.github.com>
2025-04-07 16:38:41 +0800

    update env check

--- README.md
+++ README.md
@@ -75,8 +75,15 @@ mv face_detect_utils/resources/scrfd_500m_bnkps_shape640x640.onnx face_detect_ut
 mv scrfd_10g_kps.onnx face_detect_utils/resources/scrfd_500m_bnkps_shape640x640.onnx
 ```
 ### 2. 初始化报错  
-有较高概率是 onnxruntime-gpu 版本不匹配导致的，建议根据自己 cuda 等环境尝试更换一些版本。  
-如果难以解决，可以使用 conda 安装 cudatoolkit 环境，然后再尝试 pip 安装 onnxruntime-gpu。  
+
+有较高概率是 onnxruntime-gpu 版本不匹配导致的。  
+```bash
+python check_env/check_onnx_cuda.py
+```
+观察输出是否包括 successfully.  
+如果遇到问题，你可以尝试以下方法：
+1. 建议根据自己 cuda 等环境尝试更换一些版本。  
+2. 如果难以解决，可以使用 conda 安装 cudatoolkit 环境，然后再尝试 pip 安装 onnxruntime-gpu。  
 验证可行版本如下：
 | cudatoolkit | onnxruntime-gpu | 备注 |
 | --- | --- | --- |
---
+++ check_env/check_onnx_cuda.py
@@ -0,0 +1,32 @@
+import onnxruntime
+import numpy as np
+
+def check_gpu_usage():
+    """
+    Checks if ONNX Runtime can use the GPU by attempting to create an InferenceSession
+    with the CUDAExecutionProvider.
+
+    Returns:
+        True if GPU is likely being used, False otherwise.
+    """
+    providers = ("CUDAExecutionProvider",
+             {"device_id": 0})
+    session_options = onnxruntime.SessionOptions()
+    session_options.log_severity_level = 3
+    onnx_path = "./face_detect_utils/resources/scrfd_500m_bnkps_shape640x640.onnx"
+    onnx_session = onnxruntime.InferenceSession(onnx_path, session_options, providers=[providers])
+    print(onnx_session.get_providers())
+    return "CUDAExecutionProvider" in onnx_session.get_providers()
+
+if __name__ == "__main__":
+    if check_gpu_usage():
+        print("ONNX Runtime is successfully using the GPU.")
+    else:
+        print("ONNX Runtime is NOT using the GPU or there was an error initializing the CUDA provider.")
+        print("Please ensure that:")
+        print("- You have installed the 'onnxruntime-gpu' package.")
+        print("- You have a compatible NVIDIA GPU with appropriate drivers installed.")
+        print("- CUDA and cuDNN are installed and correctly configured in your system.")
+        print("- The versions of CUDA, cuDNN, and the NVIDIA drivers are compatible with the 'onnxruntime-gpu' version you have installed.")
+        print("- The ONNX Runtime build you are using supports CUDA.")
+        
